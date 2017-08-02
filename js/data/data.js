385ec427bf72e340134f7b9d60df80000dd6b07b
Ivan <maincorp228@gmail.com>
2017-08-02 05:42:09 +0300

    Добавление попапов, api обсуждений vk, правки по макету

--- js/data/data.js
+++ js/data/data.js
@@ -1,10 +1,10 @@
-(function() {
-	$.ajax({
-	    url : 'http://localhost:3000/js/data/content.json',
-	    type : "GET",
-	    dataType : "json",
-	    success : function(msg){
-	    	console.log(msg);
-	    }
-	});
-})();
+// (function() {
+// 	$.ajax({
+// 	    url : 'http://localhost:3000/js/data/content.json',
+// 	    type : "GET",
+// 	    dataType : "json",
+// 	    success : function(msg){
+// 	    	console.log(msg);
+// 	    }
+// 	});
+// })();
